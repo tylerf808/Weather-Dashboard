b5a3e66d0fe7dcc67ee90777d15ee7e8ff9ca348
Tyler Fabian <tylerf808@gmail.com>
2021-04-09 14:35:24 -0400

    Five day cards display the temp for that day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
-var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lon=76.6122&lat=39.2904&units=imperial&appid=63c6b2efa092c7a6d9d2e7f1b655bc65";
-var currentTemp;
+let apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lon=-76.6122&lat=39.2904&units=imperial&appid=63c6b2efa092c7a6d9d2e7f1b655bc65";
+let currentTemp;
 //Variables to hold the html elements that display the data from the api call
-var oneDayCard = document.getElementById('one-day');
+let currentCard = document.getElementById('current-day');
+let dayOne = document.getElementById('day-one');
+let dayTwo = document.getElementById('day-two');
+let dayThree = document.getElementById('day-three');
+let dayFour = document.getElementById('day-four');
+let dayFive = document.getElementById('day-five');
 
 fetch(apiUrl)
     .then(function(response) {
@@ -9,31 +14,49 @@ fetch(apiUrl)
     })
     .then(function(da) {
         genOneDay(da);
+        genFiveDay(da);
         console.log(da);
     });
 
 function genOneDay(data) {
-    var cardHead = document.createElement('h2');
-    var tempP = document.createElement('p');
-    var humP = document.createElement('p');
-    var windP = document.createElement('p');
-    var iconCode = data.current.weather[0].icon;
-    var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-    var icon = document.createElement('img');
+    let cardHead = document.createElement('h2');
+    let tempP = document.createElement('p');
+    let humP = document.createElement('p');
+    let windP = document.createElement('p');
+    let iconCode = data.current.weather[0].icon;
+    let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    let icon = document.createElement('img');
     icon.src = iconUrl;
     cardHead.innerHTML = 'Baltimore: ' + moment().format('M/D/YYYY');
     tempP.innerHTML = 'Temperature: ' + data.current.temp;
     humP.innerHTML = 'Humidity: ' + data.current.humidity;
     windP.innerHTML = 'Wind Speed: ' + data.current.wind_speed;
-    oneDayCard.appendChild(cardHead);
-    oneDayCard.appendChild(icon);
-    oneDayCard.appendChild(tempP);
-    oneDayCard.appendChild(humP);
-    oneDayCard.appendChild(windP);
+    currentCard.appendChild(cardHead);
+    currentCard.appendChild(icon);
+    currentCard.appendChild(tempP);
+    currentCard.appendChild(humP);
+    currentCard.appendChild(windP);
 }
 
 function genFiveDay(data) {
     for (let i = 0; i < 5; i++) {
-
+        let header = document.createElement('h2');
+        header.innerHTML = data.daily[i].temp.day;
+        switch (i) {
+            case 0:
+                dayOne.appendChild(header);
+                break;
+            case 1:
+                dayTwo.appendChild(header);
+                break;
+            case 2:
+                dayThree.appendChild(header);
+                break;
+            case 3:
+                dayFour.appendChild(header);
+                break;
+            default:
+                dayFive.appendChild(header);
+        }
     }
 }
