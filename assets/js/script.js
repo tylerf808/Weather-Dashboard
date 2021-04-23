9eeacf15541c235f02635042b8f8d930b86c94fd
Tyler Fabian <tylerf808@gmail.com>
2021-04-23 17:28:16 -0400

    Updated to use two API calls. One for the five day and one for current.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-let apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lon=-76.6122&lat=39.2904&units=imperial&appid=63c6b2efa092c7a6d9d2e7f1b655bc65";
+let city = 'Baltimore';
+let fiveDayApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=63c6b2efa092c7a6d9d2e7f1b655bc65";
+let currentDayApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=63c6b2efa092c7a6d9d2e7f1b655bc65";
 let currentTemp;
 //Variables to hold the html elements that display the data from the api call
 let currentCard = document.getElementById('current-day');
@@ -7,15 +9,26 @@ let dayTwo = document.getElementById('day-two');
 let dayThree = document.getElementById('day-three');
 let dayFour = document.getElementById('day-four');
 let dayFive = document.getElementById('day-five');
+let searchHistory = document.getElementById('cities');
+let searchInput = document.getElementById('search-input');
+let searchBtn = document.getElementById('search-btn');
 
-fetch(apiUrl)
+fetch(currentDayApi)
     .then(function(response) {
         return response.json();
     })
     .then(function(da) {
-        genOneDay(da);
-        genFiveDay(da);
-        console.log(da);
+        //genOneDay(da);
+        console.log('current: ', da);
+    });
+
+fetch(fiveDayApi)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(da) {
+        //genFiveDay(da);
+        console.log('five day: ', da);
     });
 
 function genOneDay(data) {
@@ -23,6 +36,8 @@ function genOneDay(data) {
     let tempP = document.createElement('p');
     let humP = document.createElement('p');
     let windP = document.createElement('p');
+    let uv = document.createElement('p');
+    uv.innerHTML = 'UV Index: ' + data.current.uvi;
     let iconCode = data.current.weather[0].icon;
     let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
     let icon = document.createElement('img');
@@ -36,34 +51,61 @@ function genOneDay(data) {
     currentCard.appendChild(tempP);
     currentCard.appendChild(humP);
     currentCard.appendChild(windP);
+    currentCard.appendChild(uv);
 }
 
 function genFiveDay(data) {
-    for (let i = 1; i < 6; i++) {
-        let header = document.createElement('h2');
-        let icon = document.createElement('img');
-        icon.src = "http://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png";
-        header.innerHTML = moment.unix(data.daily[i].dt).format('M/D/YYYY');
-        switch (i) {
-            case 1:
-                dayOne.appendChild(header);
-                dayOne.appendChild(icon);
-                break;
-            case 2:
-                dayTwo.appendChild(header);
-                dayTwo.appendChild(icon);
-                break;
-            case 3:
-                dayThree.appendChild(header);
-                dayThree.appendChild(icon);
-                break;
-            case 4:
-                dayFour.appendChild(header);
-                dayFour.appendChild(icon);
-                break;
-            default:
-                dayFive.appendChild(header);
-                dayFive.appendChild(icon);
+    for (let i = 1; i <= 40; i++) {
+        if (i % 8 == 0 || i == 1) {
+            let header = document.createElement('h2');
+            let icon = document.createElement('img');
+            let temp = document.createElement('p');
+            let humidity = document.createElement('p');
+            temp.innerHTML = 'Temp: ' + data.list[i - 1].main.temp;
+            humidity.innerHTML = 'Humidity: ' + data.list[i - 1].main.humidity;
+            icon.src = "http://openweathermap.org/img/w/" + data.list[i - 1].weather[0].icon + ".png";
+            header.innerHTML = moment.unix(data.list[i - 1].dt).format('M/D/YYYY');
+            switch (i) {
+                case 1:
+                    dayOne.appendChild(header);
+                    dayOne.appendChild(icon);
+                    dayOne.appendChild(temp);
+                    dayOne.appendChild(humidity);
+                    break;
+                case 8:
+                    dayTwo.appendChild(header);
+                    dayTwo.appendChild(icon);
+                    dayTwo.appendChild(temp);
+                    dayTwo.appendChild(humidity);
+                    break;
+                case 16:
+                    dayThree.appendChild(header);
+                    dayThree.appendChild(icon);
+                    dayThree.appendChild(temp);
+                    dayThree.appendChild(humidity);
+                    break;
+                case 24:
+                    dayFour.appendChild(header);
+                    dayFour.appendChild(icon);
+                    dayFour.appendChild(temp);
+                    dayFour.appendChild(humidity);
+                    break;
+                case 32:
+                    dayFive.appendChild(header);
+                    dayFive.appendChild(icon);
+                    dayFive.appendChild(temp);
+                    dayFive.appendChild(humidity);
+                    break;
+                default:
+
+            }
         }
     }
 }
+
+function saveCity() {
+    let input = searchInput.value;
+
+}
+
+searchBtn.onclick = saveCity;
